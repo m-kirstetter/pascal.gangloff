f5f322663fe01c9b2a3cf3fd750fb7310661b2ae
maunoxyd <maunoxyd@users.noreply.github.com>
2022-11-20 16:18:13 +0000

    deploy: 6fe274c4361436490637611b5bd66d1a5fbacb2d

--- _nuxt/static/1667209698/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpascal.gangloff\u002F_nuxt\u002Fstatic\u002F1667209698",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpascal.gangloff\u002F",assetsPath:"\u002Fpascal.gangloff\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1668961091/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpascal.gangloff\u002F_nuxt\u002Fstatic\u002F1668961091",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpascal.gangloff\u002F",assetsPath:"\u002Fpascal.gangloff\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
