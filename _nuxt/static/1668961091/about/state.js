89314a47ec783fd10089c161b4346ba48aaf3bb1
maunoxyd <maunoxyd@users.noreply.github.com>
2022-11-20 18:36:10 +0000

    deploy: 45003c8f846b0e7a5f7c2da45d539e19d3127a31

--- _nuxt/static/1668961091/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpascal.gangloff\u002F_nuxt\u002Fstatic\u002F1668961091",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fpascal.gangloff\u002F",assetsPath:"\u002Fpascal.gangloff\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
